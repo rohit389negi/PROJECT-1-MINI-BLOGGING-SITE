4224e0f7a39bc07ea33a044af4816b98700e8888
rohit389negi <rohit389negi@gmail.com>
2022-02-11 19:51:13 +0530

    new update

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -32,7 +32,7 @@ const createBlog = async function (req, res) {
         if(!isValidRequestBody(requestBody)) {
             return res.status(400).send({status: false, message: 'request body not found'})
         }
-        const {title, body, authorId, category} = requestBody;
+        const {title, body, authorId, category, tags, subcategory} = requestBody;
         if(!isValid(title)) {
             return res.status(400).send({status: false, message: 'Blog Title is required'})
         }
@@ -41,6 +41,9 @@ const createBlog = async function (req, res) {
         }
         if(!isValid(authorId)) {
             return res.status(400).send({status: false, message: 'Author id is required'})
+        }
+        if(!(authorId == req.authorId)){
+            return res.status(404).send({status:false, message:'Access denied'})
         }
         if(!isValidObjectId(authorId)) {
             return res.status(400).send({status: false, message: `${authorId} is not a valid author id`})
@@ -53,6 +56,22 @@ const createBlog = async function (req, res) {
             return res.status(400).send({status: false, message: `Author does not exit`})
         }
         const blogData = {title,body,authorId,category}
+        if(tags) {
+            if(Array.isArray(tags)) {
+                blogData['tags'] = [...tags]
+            }
+            if(Object.prototype.toString.call(tags) === "[object String]") {
+                blogData['tags'] = [ tags ]
+            }
+        }
+        if(subcategory) {
+            if(Array.isArray(subcategory)) {
+                blogData['subcategory'] = [...subcategory]
+            }
+            if(Object.prototype.toString.call(subcategory) === "[object String]") {
+                blogData['subcategory'] = [ subcategory ]
+            }
+        }
         const newBlog = await blogModel.create(blogData)
         res.status(201).send({status: true, message: 'New blog created successfully', data: newBlog})
     } catch (error) {
@@ -61,7 +80,7 @@ const createBlog = async function (req, res) {
 }
 
 //GET /blogs
-const listBlog = async function (req, res) {
+const getBlog = async function (req, res) {
     try {
         const filterQuery = {isDeleted: false, isPublished: true}
         const queryParams = req.query
@@ -98,66 +117,26 @@ const updateBlog = async function (req, res) {
     try {
         const requestBody = req.body
         const blogId = req.params.blogId
+        if(!isValidRequestBody(requestBody)) {
+            return res.status(400).send({status: false, message: 'request body not found'})
+        }
         if(!isValidObjectId(blogId)) {
             res.status(400).send({status: false, message: `${blogId} is not a valid blog id`})
             return
         }
-        if(!isValidObjectId(req.authorId)) {
-            res.status(400).send({status: false, message: `${req.authorId} is not a valid token id`})
-            return
-        }
-        const blog = await blogModel.findOne({_id: blogId, isDeleted: false, deletedAt: null })
+        const blog = await blogModel.findOne({_id: blogId, isDeleted: false})
         if(!blog) {
             res.status(404).send({status: false, message: `Blog not found`})
             return
         }
-        if(blog.authorId.toString() !== req.authorId) {
+        if(blog.authorId !== req.authorId) {
             res.status(401).send({status: false, message: `Unauthorized access! Owner info doesn't match`});
             return
         }
-        if(!isValidRequestBody(requestBody)) {
-            res.status(200).send({status: true, message: 'No paramateres passed. Blog unmodified', data: blog})
-            return
-        }
-        const {title, body, tags, category, subcategory, isPublished} = requestBody;
-        const updatedBlogData = {}
-        if(isValid(title)) {
-            if(!Object.prototype.hasOwnProperty.call(updatedBlogData, '$set')) updatedBlogData['$set'] = {}
-            updatedBlogData['$set']['title'] = title
-        }
-        if(isValid(body)) {
-            if(!Object.prototype.hasOwnProperty.call(updatedBlogData, '$set')) updatedBlogData['$set'] = {}
-            updatedBlogData['$set']['body'] = body
-        }
-        if(isValid(category)) {
-            if(!Object.prototype.hasOwnProperty.call(updatedBlogData, '$set')) updatedBlogData['$set'] = {}
-            updatedBlogData['$set']['category'] = category
-        }
-        if(isPublished !== undefined) {
-            if(!Object.prototype.hasOwnProperty.call(updatedBlogData, '$set')) updatedBlogData['$set'] = {}
-            updatedBlogData['$set']['isPublished'] = isPublished
-            updatedBlogData['$set']['publishedAt'] = isPublished ? new Date() : null
-        }
-        if(tags) {
-            if(!Object.prototype.hasOwnProperty.call(updatedBlogData, '$addToSet')) updatedBlogData['$addToSet'] = {}   
-            if(Array.isArray(tags)) {
-                updatedBlogData['$addToSet']['tags'] = { $each: [...tags]}
-            }
-            if(typeof tags === "string") {
-                updatedBlogData['$addToSet']['tags'] = tags
-            }
-        }
-        if(subcategory) {
-            if(!Object.prototype.hasOwnProperty.call(updatedBlogData, '$addToSet')) updatedBlogData['$addToSet'] = {}
-            if(Array.isArray(subcategory)) {
-                updatedBlogData['$addToSet']['subcategory'] = { $each: [...subcategory]}
-            }
-            if(typeof subcategory === "string") {
-                updatedBlogData['$addToSet']['subcategory'] = subcategory
-            }
-        }
-        const updatedBlog = await blogModel.findOneAndUpdate({_id: blogId}, updatedBlogData, {new: true})
-        res.status(200).send({status: true, message: 'Blog updated successfully', data: updatedBlog});
+        const {title, body, tags, category, subcategory} = requestBody;
+        let newBlog = await blogModel.findOneAndUpdate({ _id: blogId, isDeleted: false },
+             { title, body, $addToSet: { subcategory: subcategory, tags: tags } }, { new: true })
+        res.status(200).send({status: true, message: 'Blog updated successfully', data: newBlog});
     } catch (error) {
         res.status(500).send({status: false, message: error.message});
     }
@@ -241,5 +220,46 @@ const deleteBlogByParams = async function (req, res) {
     }
 }
 
-module.exports = {createBlog, listBlog, updateBlog, deleteBlogByID, deleteBlogByParams}
+module.exports = {createBlog, getBlog, updateBlog, deleteBlogByID, deleteBlogByParams}
+
+
+
+        // const updatedBlogData = {}
+        // if(isValid(title)) {
+        //     if(!Object.prototype.hasOwnProperty.call(updatedBlogData, '$set')) updatedBlogData['$set'] = {}
+        //     updatedBlogData['$set']['title'] = title
+        // }
+        // if(isValid(body)) {
+        //     if(!Object.prototype.hasOwnProperty.call(updatedBlogData, '$set')) updatedBlogData['$set'] = {}
+        //     updatedBlogData['$set']['body'] = body
+        // }
+        // if(isValid(category)) {
+        //     if(!Object.prototype.hasOwnProperty.call(updatedBlogData, '$set')) updatedBlogData['$set'] = {}
+        //     updatedBlogData['$set']['category'] = category
+        // }
+        // if(isPublished !== undefined) {
+        //     if(!Object.prototype.hasOwnProperty.call(updatedBlogData, '$set')) updatedBlogData['$set'] = {}
+        //     updatedBlogData['$set']['isPublished'] = isPublished
+        //     updatedBlogData['$set']['publishedAt'] = isPublished ? new Date() : null
+        // }
+        // if(tags) {
+        //     if(!Object.prototype.hasOwnProperty.call(updatedBlogData, '$addToSet')) updatedBlogData['$addToSet'] = {}   
+        //     if(Array.isArray(tags)) {
+        //         updatedBlogData['$addToSet']['tags'] = { $each: [...tags]}
+        //     }
+        //     if(typeof tags === "string") {
+        //         updatedBlogData['$addToSet']['tags'] = tags
+        //     }
+        // }
+        // if(subcategory) {
+        //     if(!Object.prototype.hasOwnProperty.call(updatedBlogData, '$addToSet')) updatedBlogData['$addToSet'] = {}
+        //     if(Array.isArray(subcategory)) {
+        //         updatedBlogData['$addToSet']['subcategory'] = { $each: [...subcategory]}
+        //     }
+        //     if(typeof subcategory === "string") {
+        //         updatedBlogData['$addToSet']['subcategory'] = subcategory
+        //     }
+        // }
+        // const updatedBlog = await blogModel.findOneAndUpdate({_id: blogId}, updatedBlogData, {new: true})
+
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,30 +1,16 @@
 const express = require('express');
-
 const router = express.Router();
+
 const authorController=require('../controllers/authorController')
 const blogController=require('../controllers/blogController')
-const middleware=require('../middlewares/middleware')
+const middleware=require('../middlewares/authorisation')
 
-// create Author API
-router.post('/authors',authorController.createAuthor )
-
-// Login API for Authorization while login.
-router.post('/login',blogController.loginAuthor)
-
-// Create Blog API
-router.post('/blogs',middleware.authorisation, blogController.createBlog )
-
-// filter API
-router.get('/filterblogs',middleware.authorisation, blogController.getBlog  )
-
-// update blog API
-router.put('/blogs/:blogId',middleware.authorisation, blogController.updateDetails)
-
-// delete blog API
-router.delete('/blogs/:blogId',middleware.authorisation, blogController.deleteBlog)
-
-// deleteByFilter API
-router.delete('/blogs',middleware.authorisation, blogController.deleteSpecific)
-
+router.post('/authors', authorController.createAuthor )
+router.post('/login', authorController.login)
+router.post('/blogs', middleware.authorisation, blogController.createBlog )
+router.get('/filterblogs', middleware.authorisation, blogController.getBlog  )
+router.put('/blogs/:blogId', middleware.authorisation, blogController.updateBlog)
+router.delete('/blogs/:blogId', middleware.authorisation, blogController.deleteBlogByID)
+router.delete('/blogs', middleware.authorisation, blogController.deleteBlogByParams)
 
 module.exports = router;   
